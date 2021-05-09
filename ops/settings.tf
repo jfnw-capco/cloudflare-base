a118fc956b8c8df7cd438c8f4e8f77dde722d256
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2021-05-09 23:25:17 +0100

    Refine settings override to tighten security & performance

--- ops/settings.tf
+++ ops/settings.tf
@@ -15,21 +15,25 @@ resource "cloudflare_zone_settings_override" "settings" {
     ipv6                     = "on"
     mirage                   = var.plan == "free" ? null : "on"
     opportunistic_encryption = "on"
-    opportunistic_onion      = "off"
+    opportunistic_onion      = "on"
     privacy_pass             = "on"
     server_side_exclude      = "on"
     tls_client_auth          = "on"
     waf                      = var.plan == "free" ? null : "on"
-    webp                     = var.plan == "free" ? null : "off"
+    polish                   = var.plan == "free" ? null : "lossless"
+    webp                     = var.plan == "free" ? null : "on"
     websockets               = "on"
     zero_rtt                 = "on"
     cname_flattening         = var.plan == "free" ? null : "flatten_at_root"
+    rocket_loader            = "on"
     ssl                      = "strict"
     min_tls_version          = "1.3"
     tls_1_3                  = "zrt"
+    pseudo_ipv4              = "add_header"
     browser_cache_ttl        = 10800
     challenge_ttl            = 900
     max_upload               = 100
+    security_level           = "high"
     minify {
       css  = "on"
       js   = "on"
